d1bf3f28df34b7bd74bafeff96d5e19f575ee9b3
Alex Warton <alexnwarton@gmail.com>
2021-10-27 09:58:36 -0700

    updated StarterPage styling added styling for large screens 992px and above

--- src/components/StarterPage.js
+++ src/components/StarterPage.js
@@ -11,6 +11,8 @@ const StarterPage = () => {
 			<p>An effects pedal is an electronic device that takes an audio signal and processes it to create different sounds. Think the crunchy tone of classic rock & roll, or the dreamy lo-fi twang of a shoegaze song.</p>
 			<h3>How can you keep track of all the pedals out there?</h3>
 			<p>There are many different types of effects pedals in the market today. While Pedal Base has some of the most popular ones, there are always creators large and small adding new pedals to the soundscape. That's where you come in! Pedal Base is an interactive site that allows you to add your favorite pedal to the database.
+			<br />
+			<br />
 			<Link to="/add-pedal" className="starterLink"> Get started Here</Link></p>
 		</div>
 	)
